8ffce56522dabda7733b55a43a07ae1e29344c1d
= <=>
2024-11-09 14:13:18 +0500

    Romove the bug

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Ali-Subhan/', // Replace this with your repo name if different
+  base: '/Ali-Subhan/' // Set this to the name of your repository
 });
