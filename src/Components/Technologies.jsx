40484c16e8473a7871308bd53a6ef7575ded6d2c
= <=>
2024-11-10 04:49:04 +0500

    adding motion to the other section

--- src/Components/Technologies.jsx
+++ src/Components/Technologies.jsx
@@ -34,7 +34,13 @@ const iconVarients = (duration) => ({
 function Technologies() {
   return (
     <div className='border-b border-neutral-800 pb-24 '>
-         <h1 className='my-20 text-center text-4xl '>Technologies </h1>
+         <motion.h1
+         whileInView={{opacity:1 , y: 0}}
+         initial = {{opacity:0 , y:-100}}
+         transition={{duration:0.5}}
+
+         
+         className='my-20 text-center text-4xl '>Technologies </motion.h1>
          <div className="flex flex-wrap items-center justify-center mt-[1pc] gap-4">
             <motion.div
             variants={iconVarients(2.5)}
