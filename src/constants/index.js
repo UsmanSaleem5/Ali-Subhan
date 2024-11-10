097a96f24386321f26c2c5146c2f97bb27950ec4
= <=>
2024-11-10 05:02:31 +0500

    change project name

--- src/constants/index.js
+++ src/constants/index.js
@@ -41,7 +41,7 @@ export const PROJECTS = [
     technologies: ["HTML", "CSS", "JavaScript", "Tailwind"],
   },
   {
-    title: "Task Management App",
+    title: "Admin Pannel",
     image: project2,
     description:
       "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
