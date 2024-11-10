51259165d16abb597b4e766866711d2dcdaac8a1
= <=>
2024-11-10 07:39:39 +0500

    making the new page of blog for admin panel projct

--- src/Components/AdminPanelBlog.jsx
+++ src/Components/AdminPanelBlog.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import { motion } from 'framer-motion'
 function AdminPanelBlog() {
   return (
-    <div>AdminPanelBlog</div>
+    <div>
+         <motion.h2
+      whileInView={{opacity:1, y: 0}}
+      initial={{ opacity: 0, y: -100 }}
+      transition={{ duration: 0.5 }}
+      className="my-20 text-center text-4xl">VPN Admin Pannel</motion.h2>    
+
+    </div>
   )
 }
 
